08542e037b99f62af3c6fd61d3eeefdb5a528e55
Yan Bein <rbeinyan@gmail.com>
2022-08-23 10:37:06 -0300

    Fix

--- src/DependencyInjection/Configuration.php
+++ src/DependencyInjection/Configuration.php
@@ -23,8 +23,8 @@ class Configuration implements ConfigurationInterface
      */
     public function getConfigTreeBuilder()
     {
-        $treeBuilder = new TreeBuilder();
-        $treeBuilder->root('cmf_resource')
+        $treeBuilder = new TreeBuilder('cmf_resource');
+        $treeBuilder->getRootNode()
             ->fixXmlConfig('repository', 'repositories')
             ->children()
                 ->arrayNode('description')
